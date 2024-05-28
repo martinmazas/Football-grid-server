1a59f5e3cbc0e3cb9e12484382ac54613c469bf9
martinmazas <mazasmartin@gmail.com>
2024-05-29 00:12:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ async function startServer() {
         const db = new DB(uri);
         await db.connectToDB();
 
-        app.listen(process.env.PORT || 3001, () => {
-            console.log(`Server running on port ${process.env.PORT}`);
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
         });
     } catch (err) {
         console.error(writeLog(`Error starting server: ${err.message}`, 'error'));
