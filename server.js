e8553aa83a10ab3a85c02524b363d01d11ce822c
martinmazas <mazasmartin@gmail.com>
2024-05-30 23:21:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,57 +4,26 @@ require('dotenv').config()
 const DB = require('./DB/DBconnection');
 const { playerRoutes } = require('./Routes/playerRoutes');
 const { paramsRoutes } = require('./Routes/paramsRoutes');
-const { writeLog } = require('./Utils/functions');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-async function startServer() {
-    try {
-        // Middleware
-        app.use(express.json());
-        app.use(express.urlencoded({ extended: true }));
-        app.use(cors());
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-        // Routes
-        app.use('/players', playerRoutes)
-        app.use('/parameters', paramsRoutes)
+// Routes
+app.use('/players', playerRoutes)
+app.use('/parameters', paramsRoutes)
 
-        // Root route
-        app.get('/', (req, res) => {
-            res.send('TicTacToe-server')
-        });
+app.get('/', (req, res) => {
+    res.send('TicTacToe-server')
+})
 
-        // Database connection and server start
-        let uri = process.env.MONGO_URI;
-        if (!uri) {
-            throw new Error('MONGO_URI is not defined in environment variables');
-        }
-
-        const db = new DB(uri)
-        await db.connectToDB()
-
-        // Set timeout for server restart
-        const TIMEOUT = 5000; // 5 seconds
-
-        const restartServer = () => {
-            console.log('Operation timeout reached. Restarting server...');
-            process.exit(1); // Exit with failure code to restart if using a process manager
-        };
-
-        // Timeout for the getParams route
-        app.use('/parameters', (req, res, next) => {
-            setTimeout(restartServer, TIMEOUT)
-            next();
-        });
-
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`)
-        })
-    } catch (err) {
-        console.error(writeLog(`Error starting server: ${err.message}`, 'error'))
-    }
-}
-
-// Start the server
-startServer()
+app.listen(PORT, async () => {
+    let uri = process.env.MONGO_URI;
+    const db = new DB(uri)
+    await db.connectToDB()
+    console.log(`Server running on port ${PORT}`)
+})
