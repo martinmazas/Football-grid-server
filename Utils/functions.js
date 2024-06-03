bff32f91c3974efef728fe9c2da9274e0cb1be1b
martinmazas <mazasmartin@gmail.com>
2024-06-03 18:10:27 +0300

    Update functions.js

--- Utils/functions.js
+++ Utils/functions.js
@@ -13,7 +13,7 @@ function objectToMap(obj) {
     const map = new Map();
     for (const [key, value] of Object.entries(obj)) {
         if (value && typeof value === 'object' && !Array.isArray(value)) {
-            map.set(key, objectToMap(value));
+            map.set(key, objectToMap(value))
         } else {
             map.set(key, value)
         }
