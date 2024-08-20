2335cd1429ee48e947e2b9738fea54e54ea41e1f
martinmazas <mazasmartin@gmail.com>
2024-08-20 18:42:58 +0300

    .

--- Utils/functions.js
+++ Utils/functions.js
@@ -129,7 +129,7 @@ module.exports = {
         const logEntry = `${timestamp} - ${type.toUpperCase()} - ${message}\n`
 
         fs.appendFile(logFile, logEntry)
-            .then(() => console.log('Log entry added'))
+            .then(() => console.log(`Log entry added in ${logDir}`))
             .catch(err => console.error('Failed to write to log file:', err))
     },
     getReqHeaders: (req) => {
