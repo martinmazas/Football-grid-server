89bcb6ca5ab5db3ebaa888bccaf52b345085b7bd
Martin Mazas <mazasmartin@gmail.com>
2024-08-20 14:33:51 +0300

    Update teamsController.js

--- Controllers/teamsController.js
+++ Controllers/teamsController.js
@@ -1,22 +1,39 @@
 const Team = require('../DB/Schemas/teamSchema')
+const { getReqHeaders, writeLog } = require('../Utils/functions')
 
 module.exports = {
     getTeams: async (req, res) => {
+        const [ua, ip] = [...getReqHeaders(req)]
+
         const teams = []
         await Team.find({})
             .then(data => data.map(team => teams.push(team)))
-            .catch(err => console.log(err))
+            .catch(err => {
+                const message = `${err} when ${ip} with UA: ${ua} tried to use Get teams function`
+                writeLog(message, 'ERROR')
+            })
 
+        const message = `Get teams function used by ${ip} with UA: ${ua}`
+        writeLog(message, 'INFO')
         return teams
     },
     addTeam: async (req, res) => {
         const { name, code, url } = { ...req.body }
+        const [ua, ip] = [...getReqHeaders(req)]
+
         const newTeam = new Team({
             name: name,
             code: code,
             url: url
         }).save()
-            .then(docs => res.send(`Team ${name} was successfully added to the DB`))
-            .catch(err => res.send(`${err}`))
+            .then(docs => {
+                const message = `Team ${name} was successfully added to the DB by ${ip} with UA: ${ua}`
+                writeLog(message, 'INFO')
+                res.send(`Team ${name} was successfully added to the DB`)
+            })
+            .catch(err => {
+                const message = `${err} when ${ip} with UA: ${ua} tried to add team ${team}`
+                writeLog(message, 'ERROR')
+            })
     }
 }
