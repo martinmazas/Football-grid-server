a227d76f00409257c49d510065c6edab6236f3c8
martinmazas <mazasmartin@gmail.com>
2026-01-06 18:35:57 -0300

    Update wordleController.ts

--- src/Controllers/wordleController.ts
+++ src/Controllers/wordleController.ts
@@ -3,7 +3,8 @@ import { writeLog } from "../utils/functions";
 
 const wordleLogs = async (req: Request, res: Response) => {
   const message = "New wordle game requested";
-  writeLog(message, req, 'INFO');
+  writeLog(message, req, "INFO");
+  res.status(200).send("Wordle started");
 };
 
 export default {
